cf8aeb03321109725b441883e86fff186065fe50
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-01-12 17:45:32 +0100

    added defautl profile picture

--- backend/quizziz/accounts/models.py
+++ backend/quizziz/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.template.defaultfilters import slugify
+from django.templatetags.static import static
 
 
 class AccountManager(BaseUserManager):
@@ -35,7 +36,7 @@ class AccountManager(BaseUserManager):
 
 
 class Account(AbstractBaseUser, PermissionsMixin):
-    DEFAULT_PROFILE_PICTURE = 'images/DEFAULT_PROFILE_PICTURE.png'
+    DEFAULT_PROFILE_PICTURE = static('DEFAULT_PROFILE_PICTURE.png')
 
     email = models.EmailField(max_length=80, unique=True)
     username = models.CharField(max_length=20, unique=True)
