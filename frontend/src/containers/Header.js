6d73bb9840aa287e97bcfdb180ef584fcaaa37a0
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-01-18 15:21:42 +0100

    updated logo

--- frontend/src/containers/Header.js
+++ frontend/src/containers/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
             <header className="site-header">
                 <div className="mobile-nav">
                     <Link to="/" className="logo__link">
-                        <img src={logo} alt="quizziz" width="225" height="350" className="logo__img"></img>
+                        <img src={logo} alt="quizziz" width="150" height="30" className="logo__img"></img>
                     </Link>
                     <div className="nav-btn" onClick={this.toggleNavigation}>
                         <span className="nav-btn__burger"></span>
