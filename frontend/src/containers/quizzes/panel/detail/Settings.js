d107163b9e3cc25c2ef17b2137283741253d0045
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-01-30 15:06:20 +0100

    changed titles

--- frontend/src/containers/quizzes/panel/detail/Settings.js
+++ frontend/src/containers/quizzes/panel/detail/Settings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import SettingsForm from '../../../../components/quizzes/panel/detail/SettingsForm'
+import Title from '../../../../common/Title'
 
 class Settings extends Component {
     static propTypes = {
@@ -9,11 +10,15 @@ class Settings extends Component {
 
     render() {
         return (
-            <div className="card">
-                <div className="card__body">
-                    <SettingsForm data={this.props.data} />
+            <>
+                <Title title={`${this.props.data.title} - Quiz Settings`} />
+
+                <div className="card">
+                    <div className="card__body">
+                        <SettingsForm data={this.props.data} />
+                    </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
