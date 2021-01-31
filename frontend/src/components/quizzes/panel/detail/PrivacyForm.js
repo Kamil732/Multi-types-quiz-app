804f1114bb3197d2b4140b80aaab9ba38d8e50b2
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-01-31 13:52:45 +0100

    added row & cols

--- frontend/src/components/quizzes/panel/detail/PrivacyForm.js
+++ frontend/src/components/quizzes/panel/detail/PrivacyForm.js
@@ -85,185 +85,189 @@ class PrivacyForm extends Component {
 
         return (
             <form onSubmit={this.onSubmit}>
-                {
-                    errors.detail ? (
-                        <div className="message-box error">
-                            <p className="message-box__text">{errors.detail}</p>
+                <div className="row">
+                    {
+                        errors.detail ? (
+                            <div className="message-box error">
+                                <p className="message-box__text">{errors.detail}</p>
+                            </div>
+                        ) : ''
+                    }
+
+                    <div className="col col-sm-6">
+                        {
+                            errors.ask_name ? (
+                                <div className="message-box error">
+                                    {
+                                        errors.ask_name.map((error, index) => (
+                                            <p className="message-box__text" key={index}>{error}</p>
+                                        ))
+                                    }
+                                </div>
+                            ) : ''
+                        }
+                        <div className="form-inline">
+                            <label className="form-inline__label" htmlFor="ask_name">Ask about name:</label>
+                            <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_name">
+                                <input
+                                    type="radio"
+                                    id="ask_name__true"
+                                    name="ask_name"
+                                    value="true"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_name === true}
+                                />
+                                <label htmlFor="ask_name__true">Yes</label>
+
+                                <input
+                                    type="radio"
+                                    id="ask_name__false"
+                                    name="ask_name"
+                                    value="false"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_name === false}
+                                />
+                                <label htmlFor="ask_name__false">No</label>
+                            </div>
                         </div>
-                    ) : ''
-                }
 
+                        {
+                            errors.ask_email ? (
+                                <div className="message-box error">
+                                    {
+                                        errors.ask_email.map((error, index) => (
+                                            <p className="message-box__text" key={index}>{error}</p>
+                                        ))
+                                    }
+                                </div>
+                            ) : ''
+                        }
+                        <div className="form-inline">
+                            <label className="form-inline__label" htmlFor="ask_email">Ask about email:</label>
+                            <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_email">
+                                <input
+                                    type="radio"
+                                    id="ask_email__true"
+                                    name="ask_email"
+                                    value="true"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_email === true}
+                                />
+                                <label htmlFor="ask_email__true">Yes</label>
 
-                {
-                    errors.ask_name ? (
-                        <div className="message-box error">
-                            {
-                                errors.ask_name.map((error, index) => (
-                                    <p className="message-box__text" key={index}>{error}</p>
-                                ))
-                            }
+                                <input
+                                    type="radio"
+                                    id="ask_email__false"
+                                    name="ask_email"
+                                    value="false"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_email === false}
+                                />
+                                <label htmlFor="ask_email__false">No</label>
+                            </div>
                         </div>
-                    ) : ''
-                }
-                <div className="form-inline">
-                    <label className="form-inline__label" htmlFor="ask_name">Ask about name:</label>
-                    <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_name">
-                        <input
-                            type="radio"
-                            id="ask_name__true"
-                            name="ask_name"
-                            value="true"
-                            onChange={this.onChangeRadio}
-                            checked={ask_name === true}
-                        />
-                        <label htmlFor="ask_name__true">Yes</label>
 
-                        <input
-                            type="radio"
-                            id="ask_name__false"
-                            name="ask_name"
-                            value="false"
-                            onChange={this.onChangeRadio}
-                            checked={ask_name === false}
-                        />
-                        <label htmlFor="ask_name__false">No</label>
+                        {
+                            errors.ask_gender ? (
+                                <div className="message-box error">
+                                    {
+                                        errors.ask_gender.map((error, index) => (
+                                            <p className="message-box__text" key={index}>{error}</p>
+                                        ))
+                                    }
+                                </div>
+                            ) : ''
+                        }
+                        <div className="form-inline">
+                            <label className="form-inline__label" htmlFor="ask_gender">Ask about gender:</label>
+                            <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_gender">
+                                <input
+                                    type="radio"
+                                    id="ask_gender__true"
+                                    name="ask_gender"
+                                    value="true"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_gender === true}
+                                />
+                                <label htmlFor="ask_gender__true">Yes</label>
+
+                                <input
+                                    type="radio"
+                                    id="ask_gender__false"
+                                    name="ask_gender"
+                                    value="false"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_gender === false}
+                                />
+                                <label htmlFor="ask_gender__false">No</label>
+                            </div>
+                        </div>
+
+                        {
+                            errors.ask_opinion ? (
+                                <div className="message-box error">
+                                    {
+                                        errors.ask_opinion.map((error, index) => (
+                                            <p className="message-box__text" key={index}>{error}</p>
+                                        ))
+                                    }
+                                </div>
+                            ) : ''
+                        }
+                        <div className="form-inline">
+                            <label className="form-inline__label" htmlFor="ask_opinion">Ask about opinion:</label>
+                            <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_opinion">
+                                <input
+                                    type="radio"
+                                    id="ask_opinion__true"
+                                    name="ask_opinion"
+                                    value="true"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_opinion === true}
+                                />
+                                <label htmlFor="ask_opinion__true">Yes</label>
+
+                                <input
+                                    type="radio"
+                                    id="ask_opinion__false"
+                                    name="ask_opinion"
+                                    value="false"
+                                    onChange={this.onChangeRadio}
+                                    checked={ask_opinion === false}
+                                />
+                                <label htmlFor="ask_opinion__false">No</label>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                {
-                    errors.ask_email ? (
-                        <div className="message-box error">
-                            {
-                                errors.ask_email.map((error, index) => (
-                                    <p className="message-box__text" key={index}>{error}</p>
-                                ))
-                            }
+                    <div className="col col-sm-6">
+                        {
+                            errors.password ? (
+                                <div className="message-box error">
+                                    {
+                                        errors.password.map((error, index) => (
+                                            <p className="message-box__text" key={index}>{error}</p>
+                                        ))
+                                    }
+                                </div>
+                            ) : ''
+                        }
+                        <div className="form-inline">
+                            <label className="form-inline__label" htmlFor="password">Password:</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={password}
+                                onChange={this.onChange}
+                                className={`form-inline__input ${is_published ? 'btn__disabled' : ''}`}
+                                readOnly={is_published}
+                            />
+                            <small className="help-text">
+                                <span className="help-text__optional">OPTIONAL</span>
+                                To start solving your quiz, users will have to pass the password. Leave this field empty if you don't want require password.
+                            </small>
                         </div>
-                    ) : ''
-                }
-                <div className="form-inline">
-                    <label className="form-inline__label" htmlFor="ask_email">Ask about email:</label>
-                    <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_email">
-                        <input
-                            type="radio"
-                            id="ask_email__true"
-                            name="ask_email"
-                            value="true"
-                            onChange={this.onChangeRadio}
-                            checked={ask_email === true}
-                        />
-                        <label htmlFor="ask_email__true">Yes</label>
-
-                        <input
-                            type="radio"
-                            id="ask_email__false"
-                            name="ask_email"
-                            value="false"
-                            onChange={this.onChangeRadio}
-                            checked={ask_email === false}
-                        />
-                        <label htmlFor="ask_email__false">No</label>
                     </div>
-                </div>
-
-                {
-                    errors.ask_gender ? (
-                        <div className="message-box error">
-                            {
-                                errors.ask_gender.map((error, index) => (
-                                    <p className="message-box__text" key={index}>{error}</p>
-                                ))
-                            }
-                        </div>
-                    ) : ''
-                }
-                <div className="form-inline">
-                    <label className="form-inline__label" htmlFor="ask_gender">Ask about gender:</label>
-                    <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_gender">
-                        <input
-                            type="radio"
-                            id="ask_gender__true"
-                            name="ask_gender"
-                            value="true"
-                            onChange={this.onChangeRadio}
-                            checked={ask_gender === true}
-                        />
-                        <label htmlFor="ask_gender__true">Yes</label>
-
-                        <input
-                            type="radio"
-                            id="ask_gender__false"
-                            name="ask_gender"
-                            value="false"
-                            onChange={this.onChangeRadio}
-                            checked={ask_gender === false}
-                        />
-                        <label htmlFor="ask_gender__false">No</label>
-                    </div>
-                </div>
-
-                {
-                    errors.ask_opinion ? (
-                        <div className="message-box error">
-                            {
-                                errors.ask_opinion.map((error, index) => (
-                                    <p className="message-box__text" key={index}>{error}</p>
-                                ))
-                            }
-                        </div>
-                    ) : ''
-                }
-                <div className="form-inline">
-                    <label className="form-inline__label" htmlFor="ask_opinion">Ask about opinion:</label>
-                    <div className={`switch-btn ${is_published ? 'btn__disabled' : ''}`} id="ask_opinion">
-                        <input
-                            type="radio"
-                            id="ask_opinion__true"
-                            name="ask_opinion"
-                            value="true"
-                            onChange={this.onChangeRadio}
-                            checked={ask_opinion === true}
-                        />
-                        <label htmlFor="ask_opinion__true">Yes</label>
-
-                        <input
-                            type="radio"
-                            id="ask_opinion__false"
-                            name="ask_opinion"
-                            value="false"
-                            onChange={this.onChangeRadio}
-                            checked={ask_opinion === false}
-                        />
-                        <label htmlFor="ask_opinion__false">No</label>
-                    </div>
-                </div>
-
-                {
-                    errors.password ? (
-                        <div className="message-box error">
-                            {
-                                errors.password.map((error, index) => (
-                                    <p className="message-box__text" key={index}>{error}</p>
-                                ))
-                            }
-                        </div>
-                    ) : ''
-                }
-                <div className="form-inline">
-                    <label className="form-inline__label" htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={this.onChange}
-                        className={`form-inline__input ${is_published ? 'btn__disabled' : ''}`}
-                        readOnly={is_published}
-                    />
-                    <small className="help-text">
-                        <span className="help-text__optional">OPTIONAL</span>
-                        To start solving your quiz, users will have to pass the password. Leave this field empty if you don't want require password.
-                    </small>
                 </div>
 
                 <div className="inline-btns f-w">
