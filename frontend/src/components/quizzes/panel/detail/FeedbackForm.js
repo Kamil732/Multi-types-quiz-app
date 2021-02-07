23b6ae9617f4e34e6fb6dd3d5e866f5352ed4930
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-02-07 16:57:19 +0100

    added timer & quiz feedback

---
+++ frontend/src/components/quizzes/panel/detail/FeedbackForm.js
@@ -0,0 +1,257 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
+import axios from 'axios'
+import { clearErrors, addError } from '../../../../redux/actions/errors'
+
+import Textarea from '../../../Textarea'
+
+class FeedbackForm extends Component {
+	static propTypes = {
+		ask_name: PropTypes.bool,
+		ask_email: PropTypes.bool,
+		ask_gender: PropTypes.bool,
+		ask_opinion: PropTypes.bool,
+		author_slug: PropTypes.string.isRequired,
+		quiz_slug: PropTypes.string.isRequired,
+		errors: PropTypes.object,
+		clearErrors: PropTypes.func.isRequired,
+	}
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			name: '',
+			email: '',
+			gender: '',
+			ask_opinion: '',
+		}
+
+		this.onSubmit = this.onSubmit.bind(this)
+	}
+
+	onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+
+	onSubmit = (e) => {
+		e.preventDefault()
+		const { author_slug, quiz_slug } = this.props
+		const { name, email, gender, opinion } = this.state
+
+		const config = {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'Accept-Language': 'en',
+			},
+		}
+
+		const body = JSON.stringify({ name, email, gender, opinion })
+
+		axios
+			.post(
+				`${process.env.REACT_APP_API_URL}/quizzes/${author_slug}/${quiz_slug}/feedback/`,
+				body,
+				config
+			)
+			.then(() =>
+				this.props.history.push(`/quizzes/${author_slug}/${quiz_slug}`)
+			)
+			.catch((err) => {
+				if (err.response)
+					this.props.addError(err.response.data, err.response.status)
+			})
+	}
+
+	render() {
+		const {
+			ask_name,
+			ask_email,
+			ask_gender,
+			ask_opinion,
+			errors,
+		} = this.props
+		const { name, email, gender, opinion } = this.state
+
+		return (
+			<form onSubmit={this.onSubmit} className="auth-form">
+				{errors.detail ? (
+					<div className="message-box error">
+						<p className="message-box__text">{errors.detail}</p>
+					</div>
+				) : null}
+
+				{ask_name ? (
+					<>
+						{errors.ask_name ? (
+							<div className="message-box error">
+								{errors.ask_name.map((error, index) => (
+									<p
+										className="message-box__text"
+										key={index}
+									>
+										{error}
+									</p>
+								))}
+							</div>
+						) : null}
+
+						<div className="form-inline">
+							<label
+								htmlFor="name"
+								className="form-inline__label"
+							>
+								Name:
+							</label>
+							<input
+								type="text"
+								id="name"
+								name="name"
+								className="form-inline__input"
+								onChange={this.onChange}
+								value={name}
+								required
+							/>
+						</div>
+					</>
+				) : null}
+
+				{ask_email ? (
+					<>
+						{errors.ask_email ? (
+							<div className="message-box error">
+								{errors.ask_email.map((error, index) => (
+									<p
+										className="message-box__text"
+										key={index}
+									>
+										{error}
+									</p>
+								))}
+							</div>
+						) : null}
+
+						<div className="form-inline">
+							<label
+								htmlFor="email"
+								className="form-inline__label"
+							>
+								Email:
+							</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								className="form-inline__input"
+								onChange={this.onChange}
+								value={email}
+								required
+							/>
+						</div>
+					</>
+				) : null}
+
+				{ask_gender ? (
+					<>
+						{errors.ask_gender ? (
+							<div className="message-box error">
+								{errors.ask_gender.map((error, index) => (
+									<p
+										className="message-box__text"
+										key={index}
+									>
+										{error}
+									</p>
+								))}
+							</div>
+						) : null}
+
+						<div className="form-inline">
+							<label
+								htmlFor="gender"
+								className="form-inline__label"
+							>
+								Gender:
+							</label>
+
+							<div className="switch-btn" id="gender">
+								<input
+									type="radio"
+									id="gender__woman"
+									name="gender"
+									value="woman"
+									onChange={this.onChange}
+									checked={gender === 'woman'}
+									required
+								/>
+								<label htmlFor="gender__woman">Woman</label>
+
+								<input
+									type="radio"
+									id="gender__man"
+									name="gender"
+									value="man"
+									onChange={this.onChange}
+									checked={gender === 'man'}
+									required
+								/>
+								<label htmlFor="gender__man">Man</label>
+							</div>
+						</div>
+					</>
+				) : null}
+
+				{ask_opinion ? (
+					<>
+						{errors.ask_opinion ? (
+							<div className="message-box error">
+								{errors.ask_opinion.map((error, index) => (
+									<p
+										className="message-box__text"
+										key={index}
+									>
+										{error}
+									</p>
+								))}
+							</div>
+						) : null}
+
+						<div className="form-inline">
+							<label
+								htmlFor="opinion"
+								className="form-inline__label"
+							>
+								Opinion:
+							</label>
+							<Textarea
+								id="opinion"
+								name="opinion"
+								className="form-inline__input"
+								onChange={this.onChange}
+								value={opinion}
+							/>
+						</div>
+					</>
+				) : null}
+
+				<button className="btn btn__submit">Send Feedback</button>
+			</form>
+		)
+	}
+}
+
+const mapStateToProps = (state) => ({
+	errors: state.errors.messages,
+})
+
+const mapDispatchToProps = {
+	clearErrors,
+	addError,
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(withRouter(FeedbackForm))
--- frontend/src/containers/quizzes/detail/Start.js
+++ frontend/src/containers/quizzes/detail/Start.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import Title from '../../../common/Title'
+
 import axios from 'axios'
 
 import CircleLoader from '../../../components/loaders/CircleLoader'
 import OnePageQuiz from './OnePageQuiz'
 import MultiPageQuiz from './MultiPageQuiz'
+import FeedbackForm from '../../../components/quizzes/panel/detail/FeedbackForm'
 
 class Start extends Component {
 	static propTypes = {
@@ -21,6 +22,7 @@ class Start extends Component {
 		this.state = {
 			loading: true,
 			questions: [],
+			timer: 0,
 			finished_data: {},
 		}
 
@@ -34,12 +36,12 @@ class Start extends Component {
 
 		axios
 			.get(data.questions)
-			.then((res) =>
+			.then((res) => {
 				this.setState({
 					loading: false,
 					questions: res.data,
 				})
-			)
+			})
 			.catch((err) =>
 				this.setState({
 					loading: false,
@@ -48,7 +50,15 @@ class Start extends Component {
 			)
 	}
 
-	componentDidMount = () => this.getQuestions()
+	componentDidMount = () => {
+		this.getQuestions()
+
+		this.timer = setInterval(
+			() =>
+				this.setState((prevState) => ({ timer: prevState.timer + 1 })),
+			1000
+		)
+	}
 
 	componentDidUpdate(prevProps, _) {
 		if (prevProps.data !== this.props.data) this.getQuestions()
@@ -99,46 +109,82 @@ class Start extends Component {
 			)
 			.then((res) => {
 				this.setState({ finished_data: res.data })
-				window.scrollTo(0, 0)
+				clearInterval(this.timer)
+
+				window.scrollTo({ top: 0, behavior: `smooth` })
 			})
 	}
 
 	render() {
 		const { data } = this.props
-		const { loading, questions, finished_data } = this.state
+		const { loading, questions, timer, finished_data } = this.state
 
 		if (loading === true) return <CircleLoader />
+
+		let result
+		if (finished_data.section === 'knowledge_quiz')
+			result = (
+				<>
+					On {questions.length} questions you have answered{' '}
+					{finished_data.correctAnswers} corrctly
+				</>
+			)
 
 		return (
 			<>
 				<Title title={`Start Quiz - ${data.title}`} />
 
-				<form onSubmit={this.onSubmit}>
-					{data.one_page_questions === true ? (
-						<OnePageQuiz
-							questions={questions}
-							section={data.section.name}
-							finishedData={finished_data}
-						/>
-					) : (
-						<MultiPageQuiz
-							questions={questions}
-							section={data.section.name}
-							finishedData={finished_data}
-						/>
-					)}
+				{Object.keys(finished_data).length > 0 ? (
+					<div className="card">
+						<div className="card__body">
+							<h6>You have finished quiz in {timer} seconds</h6>
+							<h3>{result}</h3>
+							{data.is_published === false ? (
+								<div className="card__body">
+									<FeedbackForm
+										ask_name={data.ask_name}
+										ask_email={data.ask_email}
+										ask_gender={data.ask_gender}
+										ask_opinion={data.ask_opinion}
+										author_slug={data.author_slug}
+										quiz_slug={data.slug}
+									/>
+								</div>
+							) : null}
+						</div>
+						<hr />
+						<div className="card__body">
+							<OnePageQuiz
+								questions={questions}
+								section={data.section.name}
+								finishedData={finished_data}
+							/>
+						</div>
+					</div>
+				) : (
+					<form onSubmit={this.onSubmit}>
+						{data.one_page_questions === true ? (
+							<OnePageQuiz
+								questions={questions}
+								section={data.section.name}
+								finishedData={finished_data}
+							/>
+						) : (
+							<MultiPageQuiz
+								questions={questions}
+								section={data.section.name}
+								finishedData={finished_data}
+							/>
+						)}
 
-					<button className="btn btn__submit btn__contrast">
-						Finish
-					</button>
-				</form>
+						<div className="card__footer">
+							<button className="btn btn__submit">Finish</button>
+						</div>
+					</form>
+				)}
 			</>
 		)
 	}
 }
 
-const mapStateToProps = (state) => ({})
-
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Start)
+export default Start
