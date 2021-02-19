73aa862bf4b39c9c935b1d299e9766139ef8e289
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-02-19 19:43:20 +0100

    fixed issues & cleaned code

--- frontend/src/components/quizzes/panel/detail/PunctationList.js
+++ frontend/src/components/quizzes/panel/detail/PunctationList.js
@@ -19,166 +19,85 @@ class PunctationList extends Component {
 		this.data = Object.values(this.props.punctations)
 		this.dataRefs = []
 
-		this.recalculateAddedRatings = this.recalculateAddedRatings.bind(this)
-		this.recalculateDeletedRatings = this.recalculateDeletedRatings.bind(
-			this
-		)
 		this.recalculateRatings = this.recalculateRatings.bind(this)
 	}
 
-	recalculateAddedRatings = (index = this.dataRefs.length - 1) => {
-		const to_score = this.dataRefs[index].to_score
-		const from_score = this.dataRefs[index].from_score
+	recalculateRatings() {
+		const { max_score, punctations, hasChanged } = this.props
 
-		if (from_score.value > to_score.value)
-			// Minus 1 from from_score
-			from_score.value = parseInt(from_score.value) - 1
+		const shown = this.dataRefs.length - 1
+		let expectedFrom = 0
 
-		if (this.dataRefs[index - 1]) {
-			const previous_to_score = this.dataRefs[index - 1].to_score
+		for (let i = 0; i < shown; i++) {
+			const { from_score, to_score } = this.dataRefs[i]
 
-			// Set previous_to_score to from_score minus 1
-			previous_to_score.value = parseInt(from_score.value) - 1
+			from_score.value = expectedFrom
+			let expectedTo = parseInt(to_score.value)
 
-			// Set to_score MIN to from_score
-			to_score.min = from_score.value
-
-			// Set previous_to_score MAX to to_score minus 1
-			previous_to_score.max = parseInt(to_score.value) - 1
-
-			// Recursion to validate other inputs
-			this.recalculateAddedRatings(index - 1)
-		}
-	}
-
-	recalculateDeletedRatings = (index = this.dataRefs.length - 1) => {
-		const to_score = this.dataRefs[index].to_score
-		const from_score = this.dataRefs[index].from_score
-
-		// Add 1 to to_score
-		to_score.value = parseInt(to_score.value) + 1
-
-		if (from_score.value > to_score.value)
-			// minus 1 from from_score
-			from_score.value = parseInt(from_score.value) - 1
-
-		if (this.dataRefs[index - 1]) {
-			const previous_to_score = this.dataRefs[index - 1].to_score
-			const previous_from_score = this.dataRefs[index - 1].from_score
-
-			if (from_score.value > previous_to_score.value)
-				// Minus 1 from previous_to_score
-				previous_to_score.value = parseInt(previous_to_score.value) - 1
-
-			// Set to_score MIN to from_score VALUE
-			to_score.min = from_score.value
-
-			if (previous_to_score.value >= from_score.value)
-				// Set previous_from_score to previous_from_score minus 1
-				previous_from_score.value =
-					parseInt(previous_from_score.value) - 1
-
-			// Recursion to validate other inputs
-			this.recalculateDeletedRatings(index - 1)
+			if (expectedTo < expectedFrom) {
+				expectedTo = expectedFrom
+				to_score.value = expectedTo
+			}
+			expectedFrom = expectedTo + 1
+			if (expectedFrom > max_score) {
+				expectedFrom = max_score
+			}
 		}
 
-		if (this.dataRefs[index + 1]) {
-			const next_to_score = this.dataRefs[index + 1].to_score
-			const next_from_score = this.dataRefs[index + 1].from_score
+		let expectedTo = max_score
+		for (let i = shown; i >= 0; --i) {
+			const { from_score, to_score } = this.dataRefs[i]
 
-			// Set to_score MAX to next_to_score MAX
-			to_score.max =
-				next_to_score.value === next_from_score.value
-					? parseInt(next_to_score.max)
-					: parseInt(next_to_score.max) - 1
-		} else {
-			// Its the last
-			to_score.value = this.props.max_score
-			to_score.max = this.props.max_score
-		}
-	}
-
-	componentDidUpdate(prevProps, _) {
-		if (prevProps.punctations.length < this.props.punctations.length)
-			this.recalculateAddedRatings()
-		else if (prevProps.punctations.length > this.props.punctations.length) {
-			// Delete unnecessary data
-			for (
-				let i = 0;
-				i <
-				prevProps.punctations.length - this.props.punctations.length;
-				i++
-			) {
-				this.dataRefs.pop()
-				this.data.pop()
+			to_score.value = expectedTo
+			let expectedFrom = parseInt(from_score.value)
+			if (expectedFrom > expectedTo) {
+				expectedFrom = expectedTo
+				from_score.value = expectedFrom
 			}
 
-			this.recalculateDeletedRatings()
+			expectedTo = expectedFrom - 1 >= 0 ? expectedFrom - 1 : 0
 		}
-	}
-
-	recalculateRatings = (e) => {
-		const target = e.target ? e.target : e
-		const id = parseInt(target.getAttribute('data-id'))
-
-		const from_score = this.dataRefs[id].from_score
-		const to_score = this.dataRefs[id].to_score
 
 		// Set the hasChanged
-		if (this.props.hasChanged) {
+		if (hasChanged && punctations.length === this.data.length) {
 			// Update data
-			this.data[id] = {
-				from_score: parseInt(from_score.value),
-				to_score: parseInt(to_score.value),
-				summery: this.dataRefs[id].summery.value(), // .value() is function because summery is component
-			}
+			for (let i = 0; i < this.dataRefs.length; i++)
+				this.data[i] = {
+					from_score: parseInt(this.dataRefs[i].from_score.value),
+					to_score: parseInt(this.dataRefs[i].to_score.value),
+					summery: this.dataRefs[i].summery.value(), // .value() is function because summery is component
+				}
 
 			// array of booleans, true if object has change otherwise false
 			const hasChangedArray = this.data.map(
 				(_, index) =>
-					!objectsEquals(
-						this.props.punctations[index],
-						this.data[index]
-					)
+					!objectsEquals(punctations[index], this.data[index])
 			)
 
 			// If true in array than the form has changed
-			this.props.hasChanged(
+			hasChanged(
 				hasChangedArray.some((hasChanged) => hasChanged === true)
 			)
 		}
+	}
 
-		if (from_score.value > to_score.value)
-			// Add 1 to to_score
-			to_score.value = parseInt(to_score.value) + 1
-
-		if (this.dataRefs[id + 1]) {
-			const next_from_score = this.dataRefs[id + 1].from_score
-			const next_to_score = this.dataRefs[id + 1].to_score
-
-			// Set next_from_score to to_score plus 1
-			next_from_score.value = parseInt(to_score.value) + 1
-
-			// Set to_score MIN to from_score VALUE
-			to_score.min = from_score.value
-
-			// Set to_score MAX to next_to_score MAX minus 1
-			to_score.max = parseInt(next_to_score.max) - 1
-
-			if (this.dataRefs[id - 1]) {
-				const previous_to_score = this.dataRefs[id - 1].to_score
-
-				// If previous_to_score is greater than or equal to from_score then add 1 to form_score
-				if (previous_to_score.value >= from_score.value)
-					from_score.value = parseInt(from_score.value) + 1
+	componentDidUpdate(prevProps, _) {
+		if (prevProps.punctations.length !== this.props.punctations.length) {
+			if (prevProps.punctations.length > this.props.punctations.length) {
+				// Delete unnecessary data
+				for (
+					let i = 0;
+					i <
+					prevProps.punctations.length -
+						this.props.punctations.length;
+					i++
+				) {
+					this.dataRefs.pop()
+					this.data.pop()
+				}
 			}
 
-			// Recursion to validate other inputs
-			this.recalculateRatings(next_to_score)
-		} else {
-			// If its the last
-			to_score.value = this.props.max_score
-			to_score.max = this.props.max_score
+			this.recalculateRatings()
 		}
 	}
 
@@ -219,22 +138,15 @@ class PunctationList extends Component {
 									type="text"
 									name="to_score"
 									className="form-inline__input number-field__input"
-									min={punctation.from_score}
-									max={
-										punctations[index + 1]
-											? punctations[index + 1].to_score -
-											  1
-											: this.props.max_score
-									}
-									defaultValue={punctation.to_score}
-									required
-									readOnly
 									ref={(ref) =>
 										(this.dataRefs[index] = {
 											...this.dataRefs[index],
 											to_score: ref,
 										})
 									}
+									defaultValue={punctation.to_score}
+									required
+									readOnly
 								/>
 								<div className="number-field__btns">
 									<button
@@ -246,11 +158,10 @@ class PunctationList extends Component {
 												index
 											].to_score
 
-											if (to_score.value < to_score.max)
-												to_score.value =
-													parseInt(to_score.value) + 1
+											to_score.value =
+												parseInt(to_score.value) + 1
 
-											this.recalculateRatings(to_score)
+											this.recalculateRatings()
 										}}
 									>
 										<IoMdArrowDropup />
@@ -264,11 +175,10 @@ class PunctationList extends Component {
 												index
 											].to_score
 
-											if (to_score.value > to_score.min)
-												to_score.value =
-													parseInt(to_score.value) - 1
+											to_score.value =
+												parseInt(to_score.value) - 1
 
-											this.recalculateRatings(to_score)
+											this.recalculateRatings()
 										}}
 									>
 										<IoMdArrowDropdown />
