e0a60ea8211e40cf07b7406dc9d05684e6240b38
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-02-27 19:14:35 +0100

    added default image to image preview

--- frontend/src/components/quizzes/ImageUrlPreview.js
+++ frontend/src/components/quizzes/ImageUrlPreview.js
@@ -3,95 +3,103 @@ import PropTypes from 'prop-types'
 import axios from 'axios'
 import SpinLoader from '../loaders/SpinLoader'
 
-import { debounce } from 'lodash';
+import { debounce } from 'lodash'
 
 class ImageUrlPreview extends Component {
-    static propTypes = {
-        image_url: PropTypes.string.isRequired,
-    }
+	static propTypes = {
+		image_url: PropTypes.string.isRequired,
+		defaultImage: PropTypes.string,
+	}
 
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props)
 
-        this.state = {
-            loading: true,
-            image_url: '',
-            success: null,
-        }
+		this.state = {
+			loading: true,
+			image_url: '',
+			success: null,
+		}
 
-        this.setImageUrl = debounce(this.setImageUrl.bind(this), 500)
-    }
+		this.setImageUrl = debounce(this.setImageUrl.bind(this), 500)
+	}
 
-    async setImageUrl() {
-        this.setState({ loading: true })
-        const { image_url } = this.props
+	async setImageUrl() {
+		this.setState({ loading: true })
+		const { image_url } = this.props
 
-        const config = {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Accept-Language': 'pl',
-            }
-        }
+		const config = {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'Accept-Language': 'pl',
+			},
+		}
 
-        const body = JSON.stringify({ image_url })
+		const body = JSON.stringify({ image_url })
 
-        try {
-            const res = await axios.post(`${process.env.REACT_APP_API_URL}/image-url-validation/`, body, config)
+		try {
+			const res = await axios.post(
+				`${process.env.REACT_APP_API_URL}/image-url-validation/`,
+				body,
+				config
+			)
 
-            this.setState({
-                loading: false,
-                image_url: res.data.image_url,
-                success: res.data.success,
-            })
-        } catch (err) {
-            this.setState({
-                loading: false,
-                image_url: '',
-                success: false,
-            })
-        }
-    }
+			this.setState({
+				loading: false,
+				image_url: res.data.image_url,
+				success: res.data.success,
+			})
+		} catch (err) {
+			this.setState({
+				loading: false,
+				image_url: '',
+				success: false,
+			})
+		}
+	}
 
-    componentDidMount = () => this.setImageUrl()
+	componentDidMount = () => this.setImageUrl()
 
-    componentDidUpdate(prevProps, _) {
-        if (prevProps.image_url !== this.props.image_url)
-            this.setImageUrl()
-    }
+	componentDidUpdate(prevProps, _) {
+		if (prevProps.image_url !== this.props.image_url) this.setImageUrl()
+	}
 
-    render() {
-        const { loading, image_url, success } = this.state
+	render() {
+		const { defaultImage } = this.props
+		const { loading, success } = this.state
+		let { image_url } = this.state
 
-        return (
-            <div className="img-preview">
-                <h3>Current Image:</h3>
+		if (success === false) image_url = defaultImage
 
-                {
-                    loading === true ? (
-                        <SpinLoader />
-                    ) : (
-                        <>
-                            <img
-                                src={image_url}
-                                alt=""
-                                className="img-preview__img"
-                            />
+		return (
+			<div className="img-preview">
+				<h3>Current Image:</h3>
 
-                            {
-                                success === false ? (
-                                    <div className="message-box error">
-                                        <p className="message-box__text">The URL you entered either doesn't exist or is invalid</p>
-                                    </div>
-                                ) : ''
-                            }
-                        </>
-                    )
-                }
-            </div>
-        )
-    }
+				{loading === true ? (
+					<SpinLoader />
+				) : (
+					<>
+						<img
+							src={image_url}
+							alt=""
+							className="img-preview__img"
+						/>
+
+						{success === false ? (
+							<div className="message-box error">
+								<p className="message-box__text">
+									The URL you entered either doesn't exist or
+									is invalid
+								</p>
+							</div>
+						) : (
+							''
+						)}
+					</>
+				)}
+			</div>
+		)
+	}
 }
-
 
 export default ImageUrlPreview
