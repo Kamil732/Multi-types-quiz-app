a7f17b578719ddc72bde20aefdf23cc157eb16bd
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-03-15 18:12:24 +0100

    added facebook login

--- frontend/src/containers/accounts/Auth.js
+++ frontend/src/containers/accounts/Auth.js
@@ -2,33 +2,95 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+
 import LoginForm from '../../components/accounts/auth/LoginForm'
 import RegisterForm from '../../components/accounts/auth/RegisterForm'
+import FacebookLogin from 'react-facebook-login'
+import axios from 'axios'
+import {
+	LOGIN_FAIL,
+	LOGIN_SUCCESS,
+	USER_LOADED,
+	USER_LOADING,
+} from '../../redux/actions/types'
 
 class Auth extends Component {
-    static propTypes = {
-        type: PropTypes.string.isRequired,
-        isAuthenticated: PropTypes.bool,
-    }
+	static propTypes = {
+		type: PropTypes.string.isRequired,
+		isAuthenticated: PropTypes.bool,
+		dispatch: PropTypes.func.isRequired,
+	}
 
-    render() {
-        if(this.props.isAuthenticated) return <Redirect to="/" />
+	responseFacebook = (data) => {
+		const config = {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'Accept-Language': 'en',
+			},
+		}
 
-        let form
-        if (this.props.type === 'login') form = <LoginForm />
-        else if (this.props.type === 'register') form = <RegisterForm />
-        else throw Error('Bad Type of Authentication. You must define in props either type="login" or type="register".')
+		const body = JSON.stringify({
+			provider: 'facebook',
+			access_token: data.accessToken,
+		})
 
-        return (
-            <div className="card-inline">
-                {form}
-            </div>
-        )
-    }
+		axios
+			.post(
+				`${process.env.REACT_APP_API_URL}/accounts/login/facebook/`,
+				body,
+				config
+			)
+			.then((res) => {
+				this.props.dispatch({ type: USER_LOADING })
+				this.props.dispatch({
+					type: LOGIN_SUCCESS,
+					payload: {
+						access: res.data.token,
+						refresh: res.data.refresh,
+					},
+				})
+
+				this.props.dispatch({
+					type: USER_LOADED,
+					payload: res.data.user,
+				})
+			})
+			.catch((err) => {
+				this.props.dispatch({
+					type: LOGIN_FAIL,
+				})
+			})
+	}
+
+	render() {
+		if (this.props.isAuthenticated) return <Redirect to="/" />
+
+		let form
+		if (this.props.type === 'login') form = <LoginForm />
+		else if (this.props.type === 'register') form = <RegisterForm />
+		else
+			throw Error(
+				'Bad Type of Authentication. You must define in props either type="login" or type="register".'
+			)
+
+		return (
+			<div className="card-inline">
+				{form}
+
+				<FacebookLogin
+					appId="309943713807869"
+					fields="name,first_name"
+					callback={this.responseFacebook}
+					icon="fa-facebook"
+				/>
+			</div>
+		)
+	}
 }
 
-const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated,
+const mapStateToProps = (state) => ({
+	isAuthenticated: state.auth.isAuthenticated,
 })
 
 export default connect(mapStateToProps)(Auth)
