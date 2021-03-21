6c54c9cde4e29adaff9ca3b6889ac201ff269649
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-03-21 16:48:13 +0100

    added: ads between quizzes

--- frontend/src/components/quizzes/List.js
+++ frontend/src/components/quizzes/List.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
+import Ad from '../Ad'
 import CircleLoader from '../../components/loaders/CircleLoader'
 import TextTruncate from 'react-text-truncate'
 
@@ -15,69 +16,72 @@ class List extends Component {
 		if (this.props.loading) return <CircleLoader />
 		else if (this.props.quizzes.length > 0)
 			return this.props.quizzes.map((quiz, index) => (
-				<article className="card-inline quiz-card" key={index}>
-					<Link
-						to={`/quizzes/${quiz.author_slug}/${quiz.slug}`}
-						className="card-inline__img-link"
-					>
-						<img
-							className="card-inline__img quiz-card__img"
-							src={quiz.image_url}
-							alt={quiz.title}
-						/>
-					</Link>
-
-					<section className="card-inline__body quiz-card__body">
-						<header className="quiz-card__header text-darken display-sm">
-							<span>
-								Section:&ensp;
-								<Link to={`?section=${quiz.section.name}`}>
-									{quiz.section.display_name}
-								</Link>
-							</span>
-							&ensp;&bull;&ensp;
-							<span>
-								Category:&ensp;
-								<Link
-									to={`?category__name=${quiz.category.name}`}
-								>
-									{quiz.category.display_name}
-								</Link>
-							</span>
-						</header>
-						<aside>
-							<Link
-								to={`/quizzes/${quiz.author_slug}/${quiz.slug}`}
-								className="quiz-card__link"
-							>
-								<TextTruncate
-									text={quiz.title}
-									line={2}
-									truncateText="…"
-								/>
-							</Link>
-							<TextTruncate
-								text={quiz.description}
-								element="p"
-								line={1}
-								truncateText="…"
-								containerClassName="quiz-card__description"
+				<>
+					<article className="card-inline quiz-card" key={index}>
+						<Link
+							to={`/quizzes/${quiz.author_slug}/${quiz.slug}`}
+							className="card-inline__img-link"
+						>
+							<img
+								className="card-inline__img quiz-card__img"
+								src={quiz.image_url}
+								alt={quiz.title}
 							/>
-						</aside>
-						<footer>
-							<p className="text-darken display-sm">
-								<Link to={`/profile/${quiz.author_slug}`}>
-									{quiz.author}
-								</Link>
+						</Link>
+
+						<section className="card-inline__body quiz-card__body">
+							<header className="quiz-card__header text-darken display-sm">
+								<span>
+									Section:&ensp;
+									<Link to={`?section=${quiz.section.name}`}>
+										{quiz.section.display_name}
+									</Link>
+								</span>
 								&ensp;&bull;&ensp;
 								<span>
-									Solved {quiz.solved_times} time
-									{quiz.solved_times !== 1 ? 's' : ''}
+									Category:&ensp;
+									<Link
+										to={`?category__name=${quiz.category.name}`}
+									>
+										{quiz.category.display_name}
+									</Link>
 								</span>
-							</p>
-						</footer>
-					</section>
-				</article>
+							</header>
+							<aside>
+								<Link
+									to={`/quizzes/${quiz.author_slug}/${quiz.slug}`}
+									className="quiz-card__link"
+								>
+									<TextTruncate
+										text={quiz.title}
+										line={2}
+										truncateText="…"
+									/>
+								</Link>
+								<TextTruncate
+									text={quiz.description}
+									element="p"
+									line={1}
+									truncateText="…"
+									containerClassName="quiz-card__description"
+								/>
+							</aside>
+							<footer>
+								<p className="text-darken display-sm">
+									<Link to={`/profile/${quiz.author_slug}`}>
+										{quiz.author}
+									</Link>
+									&ensp;&bull;&ensp;
+									<span>
+										Solved {quiz.solved_times} time
+										{quiz.solved_times !== 1 ? 's' : ''}
+									</span>
+								</p>
+							</footer>
+						</section>
+					</article>
+					{index % 6 === 0 ? <Ad /> : null}
+				</>
 			))
 
 		return (
